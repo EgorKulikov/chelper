4053d5a83b5e17618d43ed9462f0cf4e4fe2f33a
GitHub Action <action@github.com>
2023-02-12 07:41:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/egorkulikov/chelper/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/egorkulikov/chelper/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.egorkulikov.chelper.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.egorkulikov.chelper.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/egorkulikov/chelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/egorkulikov/chelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.egorkulikov.chelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.egorkulikov.chelper.MyBundle
 
 class MyApplicationService {
 
